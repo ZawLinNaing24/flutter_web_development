479370353abaff1f9cd285e41b00fa744ae97f4d
ZawLinNaing <zawlinnaing52@gmail.com>
2025-10-30 02:52:31 +0630

    building the core web structure

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScrollController _scrollController;
+
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
